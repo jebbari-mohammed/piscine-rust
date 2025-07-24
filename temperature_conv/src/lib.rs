4d897dccd1020b200be46cf78c0c8e8532e37b37
mojebbari <jebbari.mohammed24@ump.ac.ma>
2025-07-24 13:08:36 +0100

    tmp-conv

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,9 +1,7 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    let x = (f-32.0)*(5.0/9.0);
-    (x * 100.0).round() / 100.0
+    (f-32.0)/(9.0/5.0)
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
     (c*(9.0/5.0))+32.0
-    
 }

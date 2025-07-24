e1bc61522379ca58b3dd73e4a66b4ebf0c8603b8
mojebbari <jebbari.mohammed24@ump.ac.ma>
2025-07-24 23:20:03 +0100

    tuples

---
+++ tuples_refs/src/lib.rs
@@ -0,0 +1,11 @@
+pub fn id(student: &Student) -> u32 {
+student.0
+}
+pub struct Student(pub u32, pub String, pub String);
+pub fn first_name(student: &Student) -> &str {
+    &student.1
+}
+
+pub fn last_name(student: &Student) -> &str {
+    &student.2
+}

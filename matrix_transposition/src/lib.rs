32d1a6657a08f48558aa3f4dcf981433976ffe20
mojebbari <jebbari.mohammed24@ump.ac.ma>
2025-07-24 21:15:36 +0100

    matrix

---
+++ matrix_transposition/src/lib.rs
@@ -0,0 +1,9 @@
+#[derive(Debug)]
+pub struct Matrix (pub(u32,u32), pub(u32,u32));
+
+pub fn transpose(m: Matrix) -> Matrix {
+    Matrix (
+        (m.0.0 , m.1.0),
+        (m.0.1 , m.1.1),
+    )
+}

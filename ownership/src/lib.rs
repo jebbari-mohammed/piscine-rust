988a96362bb93ade7f496a39c26d7f04da84cef5
mojebbari <jebbari.mohammed24@ump.ac.ma>
2025-07-27 11:30:26 +0100

    owner

---
+++ ownership/src/lib.rs
@@ -0,0 +1,21 @@
+pub fn first_subword(mut s: String) -> String {
+    let mut index = 0;
+    if s.contains("_"){
+        for cha in s.chars() {
+            if cha == '_'{
+                break
+            };
+            index += 1;
+        };
+        return s[0..index].to_string()
+    } else {
+        for cha in s[1..].to_string().chars() {
+            if cha.is_uppercase() {
+                break
+            };
+            index += 1;
+        };
+        return s[0..index+1].to_string()
+    };
+    s
+}

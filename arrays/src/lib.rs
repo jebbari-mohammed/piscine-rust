c9983cbdfe2ff85d0e5fce8f5c019412bf7c7cdb
mojebbari <jebbari.mohammed24@ump.ac.ma>
2025-07-27 20:52:12 +0100

    arrays

---
+++ arrays/src/lib.rs
@@ -0,0 +1,12 @@
+pub fn sum(a: &[i32]) -> i32 {
+    let mut sum = 0;
+    for i in a {
+        sum+=i
+    };
+    sum
+}
+
+pub fn thirtytwo_tens() -> [i32; 32] {
+    let list : [i32;32] = [10;32] ;
+    list
+}

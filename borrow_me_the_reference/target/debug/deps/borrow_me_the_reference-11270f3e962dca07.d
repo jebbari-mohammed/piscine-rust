a601bdbc3b43a5781ef42604e445765fe14ebb19
mojebbari <jebbari.mohammed24@ump.ac.ma>
2025-07-30 19:20:58 +0100

    brrow

--- borrow_me_the_reference/target/debug/deps/borrow_me_the_reference-11270f3e962dca07.d
+++
@@ -1,7 +0,0 @@
-/home/mojebbari/piscine-rust/borrow_me_the_reference/target/debug/deps/libborrow_me_the_reference-11270f3e962dca07.rmeta: src/lib.rs
-
-/home/mojebbari/piscine-rust/borrow_me_the_reference/target/debug/deps/libborrow_me_the_reference-11270f3e962dca07.rlib: src/lib.rs
-
-/home/mojebbari/piscine-rust/borrow_me_the_reference/target/debug/deps/borrow_me_the_reference-11270f3e962dca07.d: src/lib.rs
-
-src/lib.rs:
--- borrow_me_the_reference/target/debug/libborrow_me_the_reference.d
+++
@@ -1,1 +0,0 @@
-/home/mojebbari/piscine-rust/borrow_me_the_reference/target/debug/libborrow_me_the_reference.rlib: /home/mojebbari/piscine-rust/borrow_me_the_reference/src/lib.rs

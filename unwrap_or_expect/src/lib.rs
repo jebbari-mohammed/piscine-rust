57ea20a4ade4b991ffc71f1c00e7225343b25357
mojebbari <jebbari.mohammed24@ump.ac.ma>
2025-07-31 19:00:17 +0100

    unwrap

---
+++ unwrap_or_expect/src/lib.rs
@@ -0,0 +1,24 @@
+pub enum Security {
+    Unknown,
+    Message,
+    Warning,
+    NotFound,
+    UnexpectedUrl,
+}
+
+pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> String {
+    match security_level {
+        Security::Unknown => server.unwrap().to_string(),
+
+        Security::Message => server.expect("ERROR: program stops").to_string(),
+
+        Security::Warning => server.unwrap_or("WARNING: check the server").to_string(),
+
+        Security::NotFound => server.unwrap_or_else(|msg| format!("Not found: {}", msg)),
+
+        Security::UnexpectedUrl => match server {
+            Ok(url) => panic!("{}", url),
+            Err(msg) => msg.to_string(),
+        },
+    }
+}

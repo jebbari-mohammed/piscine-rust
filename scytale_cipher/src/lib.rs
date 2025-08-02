3a969a07fca4905ebe0d3e2de868fc6e5c202ee7
mojebbari <jebbari.mohammed24@ump.ac.ma>
2025-08-02 23:05:33 +0100

    pig

--- scytale_cipher/src/lib.rs
+++ scytale_cipher/src/lib.rs
@@ -1,26 +1,57 @@
 pub fn scytale_cipher(message: &str, i: usize) -> String {
     let mut first = vec![];
     let mut second = vec![];
+    let mut third = vec![];
     let mut result = String::new();
+    let mut nosec = false;
+    let mut noth = false;
     for (j, ch) in message.chars().enumerate() {
         if j < i {
             first.push(ch);
-        } else {
+        } if j>=i && j<i*2 {
             second.push(ch);
         }
+        if j>=i*2 && j<i*3 {
+            third.push(ch);
+        }
     }
-    let diff = first.len() - second.len() ;
+    let mut diff = first.len().saturating_sub(second.len());
+    if diff == 0 {
+        diff = second.len().saturating_sub(third.len());
+    }
+    println!("{}",diff);
     for _j in 0..diff {
-        second.push(' ');
+        if first.len() > second.len() {
+            second.push(' ');
+        }
+        if second.len() > third.len() {
+            third.push(' ');
+        }
     }
-    // println!("{:?}",first);
-    // println!("{:?}",second);
+    println!("{:?}",first);
+    println!("{:?}",second);
+    println!("{:?}",third);
+    nosec = only_spaces(second.clone());
+    noth = only_spaces(third.clone());
 
     let len = first.len();
     for j in 0..len {
             result.push(first[j]);
-            result.push(second[j]);
+            if !nosec {
+                result.push(second[j]);
+            }
+            if !noth {
+                result.push(third[j]);
+            }
     }
 
-    result.trim_end().to_string()
+    result.trim().to_string()
+}
+pub fn only_spaces(v : Vec<char>) -> bool{
+    for ch in v {
+        if ch != ' '{
+            return false
+        };
+    }
+    true
 }
--- scytale_cipher/src/main.rs
+++ scytale_cipher/src/main.rs
@@ -1,6 +1,7 @@
 use scytale_cipher::scytale_cipher;
 
+
 fn main() {
-    println!("\"scytale Code\" size=6 -> {:?}", scytale_cipher("scytale Code", 6));
-    println!("\"scytale Code\" size=8 -> {:?}", scytale_cipher("scytale Code", 8));
+    println!("\"scytale Code\" size=6 -> {:?}", scytale_cipher("a b c", 2));
+    // println!("\"scytale Code\" size=8 -> {:?}", scytale_cipher("scytale Code", 8));
 }

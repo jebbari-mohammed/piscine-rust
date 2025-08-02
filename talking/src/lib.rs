22d8597689c4a9b31acc8f1eec1670b479e11d84
mojebbari <jebbari.mohammed24@ump.ac.ma>
2025-08-02 15:38:52 +0100

    talking

--- talking/src/lib.rs
+++ talking/src/lib.rs
@@ -1,27 +1,29 @@
 pub fn talking(text: &str) -> &str {
-    if text == "" {
-        return "Just say something!"
+    if text.trim() == "" {
+        return "Just say something!";
     }
     if everything_is_uppercase(text) {
-        if (text[(text.len()-1)..]).to_string() == "?"{
-            return "Quiet, I am thinking!"
+        if text[text.len() - 1..].to_string() == "?" {
+            return "Quiet, I am thinking!";
         } else {
-            return "There is no need to yell, calm down!"
+            return "There is no need to yell, calm down!";
         }
     } else {
-        if (text[(text.len()-1)..]).to_string() == "?"{
-            return "Sure."
+        if text[text.len() - 1..].to_string() == "?" {
+            return "Sure.";
         } else {
-            return "Interesting"
+            return "Interesting";
         }
     }
-    text
 }
-pub fn everything_is_uppercase(s : &str) -> bool {
+pub fn everything_is_uppercase(s: &str) -> bool {
+    if !s.chars().all(|c| c.is_alphabetic()) {
+        return false;
+    }
     for ch in s.chars() {
         if ch.is_lowercase() {
-            return false
-        };
+            return false;
+        }
     }
     true
 }

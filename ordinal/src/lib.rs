8ffe0139ad6d39b502a6581378ee53d8befa42cc
mojebbari <jebbari.mohammed24@ump.ac.ma>
2025-08-02 13:07:04 +0100

    ordinal

--- ordinal/src/lib.rs
+++ ordinal/src/lib.rs
@@ -1,5 +1,12 @@
 pub fn num_to_ordinal(x: u32) -> String {
     let mut num = x.to_string() ;
+    let case = &num[(num.len()-2)..num.len()];
+    let x: u32 = case.parse().unwrap();
+    let res = String::new();
+    if x <= 19 && x >= 10 {
+        num.push_str("th");
+        return num;
+    }
     for ch in num.chars().rev().next() {
         match ch {
             '1' => num.push_str("st"),
--- ordinal/src/main.rs
+++ ordinal/src/main.rs
@@ -1,7 +1,7 @@
 use ordinal::*;
 
 fn main() {
-    println!("{}", num_to_ordinal(1));
+    println!("{}", num_to_ordinal(111));
     println!("{}", num_to_ordinal(22));
     println!("{}", num_to_ordinal(43));
     println!("{}", num_to_ordinal(47));

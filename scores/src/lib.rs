bd05b11bc2f5cdd7e64865c2e5632dcafe7f9af1
mojebbari <jebbari.mohammed24@ump.ac.ma>
2025-08-02 11:56:44 +0100

    test

--- scores/src/lib.rs
+++ scores/src/lib.rs
@@ -1,27 +1,16 @@
 pub fn score(s : &str) -> u64 {
     let mut score = 0;
     for ch in s.to_uppercase().chars() {
-        if ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' || ch == 'L' || ch == 'N' || ch == 'R' || ch == 'S' || ch == 'T' {
-            score += 1;
-        };
-        if ch == 'D' || ch == 'G' {
-            score += 2;
-        };
-        if ch == 'B' || ch == 'C' || ch == 'M' || ch == 'P'  {
-            score += 3;
-        };
-        if ch == 'F' || ch == 'H' || ch == 'V' || ch == 'W' || ch == 'Y'  {
-            score += 4;
-        };
-        if ch == 'K'  {
-            score += 5;
-        };
-        if ch == 'J' || ch == 'X'  {
-            score += 8;
-        };
-        if ch == 'Q' || ch == 'Z'  {
-            score += 10;
-        };
+        score += match ch {
+            'A'| 'E'| 'I'| 'O'| 'U'| 'L'| 'N'| 'R'| 'S'| 'T' => 1,
+            'D'| 'G' => 2,
+            'B'| 'C'| 'M'| 'P' => 3,
+            'F'| 'H'| 'V'| 'W'| 'Y' => 4,
+            'K' => 5 ,
+            'J'| 'K' => 8,
+            'Q'| 'Z' => 10,
+            _=> 0
+        }
     };
     score
 }

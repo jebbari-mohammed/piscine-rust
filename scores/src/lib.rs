c052d8b66b10ec754782b28f3248bdb2486903c2
mojebbari <jebbari.mohammed24@ump.ac.ma>
2025-08-02 11:47:32 +0100

    test

---
+++ scores/src/lib.rs
@@ -0,0 +1,27 @@
+pub fn score(s : &str) -> u64 {
+    let mut score = 0;
+    for ch in s.to_uppercase().chars() {
+        if ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' || ch == 'L' || ch == 'N' || ch == 'R' || ch == 'S' || ch == 'T' {
+            score += 1;
+        };
+        if ch == 'D' || ch == 'G' {
+            score += 2;
+        };
+        if ch == 'B' || ch == 'C' || ch == 'M' || ch == 'P'  {
+            score += 3;
+        };
+        if ch == 'F' || ch == 'H' || ch == 'V' || ch == 'M' || ch == 'Y'  {
+            score += 4;
+        };
+        if ch == 'K'  {
+            score += 5;
+        };
+        if ch == 'J' || ch == 'X'  {
+            score += 8;
+        };
+        if ch == 'Q' || ch == 'Z'  {
+            score += 10;
+        };
+    };
+    score
+}

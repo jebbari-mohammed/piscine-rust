b0e88a9448a1444deaeeb5a1b1ca2b9cb0e85379
mojebbari <jebbari.mohammed24@ump.ac.ma>
2025-08-03 10:15:08 +0100

    simple_hash

---
+++ simple_hash/src/lib.rs
@@ -0,0 +1,21 @@
+use std::collections::HashMap;
+
+pub fn word_frequency_counter<'a>(words: &[&'a str]) -> HashMap<&'a str, usize> {
+    let mut map = HashMap::new();
+    for &word in words {
+        match map.get(word) {
+            Some(&count) => {
+                map.insert(word, count + 1);
+            },
+            None => {
+                map.insert(word, 1);
+            }
+        }
+    }
+    map
+}
+
+
+pub fn nb_distinct_words(frequency_count: &HashMap<&str, usize>) -> usize {
+    frequency_count.len()
+}

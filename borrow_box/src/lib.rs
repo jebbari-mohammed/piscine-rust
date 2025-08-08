e88ec6c0750315de475dec0010a921e984a7994b
mojebbari <jebbari.mohammed24@ump.ac.ma>
2025-08-08 15:46:00 +0100

    borrow_box

---
+++ borrow_box/src/lib.rs
@@ -0,0 +1,51 @@
+// let mut deleted = false;
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct GameSession {
+    pub id: u32,
+    pub p1: (String, u32),
+    pub p2: (String, u32),
+    pub nb_games: u32,
+}
+impl GameSession {
+    pub fn new(id: u32, p1_name: String, p2_name: String, nb_games: u32) -> GameSession {
+        let result = GameSession {
+            id,
+            p1 : (p1_name,0),
+            p2 : (p2_name,0),
+            nb_games, 
+        };
+        result
+    }
+
+    pub fn read_winner(&self) -> Option<&(String, u32)> {
+        if self.p1.1 > self.p2.1{
+            return Some(&self.p1)
+        }
+        if self.p2.1 > self.p1.1{
+            return Some(&self.p2)
+        }
+        None
+    }
+
+    pub fn update_score(&mut self, user_name: &str) {
+        // if !deleted {
+        if self.nb_games>0 {
+            if self.p1.0 == user_name {
+                self.p1.1 += 1;
+                self.nb_games -= 1;
+            } 
+            if self.p2.0 == user_name {
+                self.p2.1 += 1;
+                self.nb_games -= 1;
+            }
+        }
+        // }
+    }
+
+    pub fn delete(self) -> String {
+    //    deleted = true;
+    let result = format!("game deleted: id -> {}", self.id) ;
+        let owner = self;
+        result
+    }
+}

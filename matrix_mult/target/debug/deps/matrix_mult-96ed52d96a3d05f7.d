0416fe553c9d9125366e9024db6bddf2db0bdc24
mojebbari <jebbari.mohammed24@ump.ac.ma>
2025-08-16 18:55:15 +0100

    matrix_mult

--- matrix_mult/target/debug/deps/matrix_mult-96ed52d96a3d05f7.d
+++
@@ -1,7 +0,0 @@
-/home/mojebbari/piscine-rust/matrix_mult/target/debug/deps/libmatrix_mult-96ed52d96a3d05f7.rmeta: src/lib.rs
-
-/home/mojebbari/piscine-rust/matrix_mult/target/debug/deps/libmatrix_mult-96ed52d96a3d05f7.rlib: src/lib.rs
-
-/home/mojebbari/piscine-rust/matrix_mult/target/debug/deps/matrix_mult-96ed52d96a3d05f7.d: src/lib.rs
-
-src/lib.rs:
--- matrix_mult/target/debug/deps/matrix_mult-f3245b738af38160.d
+++
@@ -1,5 +0,0 @@
-/home/mojebbari/piscine-rust/matrix_mult/target/debug/deps/matrix_mult-f3245b738af38160: src/main.rs
-
-/home/mojebbari/piscine-rust/matrix_mult/target/debug/deps/matrix_mult-f3245b738af38160.d: src/main.rs
-
-src/main.rs:

e9cede89bb8cb9221cd1284622c206719db7da58
mojebbari <jebbari.mohammed24@ump.ac.ma>
2025-08-16 16:39:22 +0100

    lalgebra

---
+++ lalgebra_scalar/src/lib.rs
@@ -0,0 +1,58 @@
+pub trait Scalar {
+    fn zero() -> Self;
+    fn one() -> Self;
+}
+
+impl Scalar for f64 {
+    fn zero() -> Self {
+        0.0
+    }
+    fn one() -> Self {
+        1.0
+    }
+}
+
+impl Scalar for f32 {
+    fn zero() -> Self {
+        0.0
+    }
+    fn one() -> Self {
+        1.0
+    }
+}
+
+impl Scalar for u32 {
+    fn zero() -> Self {
+        0
+    }
+    fn one() -> Self {
+        1
+    }
+}
+
+impl Scalar for u64 {
+    fn zero() -> Self {
+        0
+    }
+    fn one() -> Self {
+        1
+    }
+}
+
+impl Scalar for i32 {
+    fn zero() -> Self {
+        0
+    }
+    fn one() -> Self {
+        1
+    }
+}
+
+impl Scalar for i64 {
+    fn zero() -> Self {
+        0
+    }
+    fn one() -> Self {
+        1
+    }
+}
